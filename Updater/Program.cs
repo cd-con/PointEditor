977f576f822249c0ab6707aa5c76820c1bc5f9cc
cd-con <qqdev@yandex.ru>
2024-02-14 11:32:53 +0300

    Реорганизация проекта. Добавлен клиппинг у Canvas. Добавлен скрипт автоматического обновления.

---
+++ Updater/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+
+const string DOWNLOAD_URL = "https://github.com/cd-con/PointEditor/releases/latest/download/PointEditor.zip";
+const string DOWNLOAD_TARGET = "Update.zip";
+string EXTRACT_TARGET = Directory.GetCurrentDirectory();
+
+
+Console.WriteLine("Killing process... (waiting 3s)");
+Process? _process = Process.GetProcessesByName("PointEditor.exe").FirstOrDefault();
+_process?.Kill();
+Thread.Sleep(3000);
+Console.WriteLine("Downloading update...");
+
+using (var client = new WebClient())
+    client.DownloadFile(DOWNLOAD_URL, DOWNLOAD_TARGET);
+
+Console.WriteLine("Updating...");
+ZipFile.ExtractToDirectory(DOWNLOAD_TARGET, EXTRACT_TARGET, true);
+Console.WriteLine("Finished!");
